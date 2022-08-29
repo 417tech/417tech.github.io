a4de37686f4e5c2c1dfee4a80fd3ae56ed4fce1a
AlanDung <sndung_alan@icloud.com>
2022-08-30 00:04:21 +0800

    Update style.css

--- scroll.js
+++ scroll.js
@@ -1,6 +1,6 @@
 
-$(document).ready(function(){
-  
+$(document).ready(function () {
+
 })
 $(window).scroll(function () {
   var main = $('.main')
@@ -15,42 +15,42 @@ $(window).scroll(function () {
     sticky.removeClass('fixed');
     main.removeClass('addspace');
   };
-});      
+});
 
 var $w = $(window).scroll(function () {
   var targetOffsetOne = $("#anchor1").offset().top;
   var targetOffsetTwo = $("#anchor2").offset().top;
   var targetOffsetThree = $("#anchor3").offset().top;
   var targetOffsetFour = $("#anchor4").offset().top;
-  if ($w.scrollTop() > targetOffsetOne && $w.scrollTop() < targetOffsetTwo-10) {
+  if ($w.scrollTop() > targetOffsetOne && $w.scrollTop() < targetOffsetTwo - 10) {
     $('#navHome').css({ "color": "burlywood" });
     $('#navVideo').css({ "color": "azure" });
     $('#navBasic').css({ "color": "azure" });
     $('#navFeatures').css({ "color": "azure" });
     $('#navContact').css({ "color": "azure" });
   }
-  if ($w.scrollTop() > targetOffsetTwo-10) {
+  if ($w.scrollTop() > targetOffsetTwo - 10) {
     $('#navHome').css({ "color": "azure" });
     $('#navVideo').css({ "color": "burlywood" });
     $('#navBasic').css({ "color": "azure" });
     $('#navFeatures').css({ "color": "azure" });
     $('#navContact').css({ "color": "azure" });
   }
-  if ($w.scrollTop() > targetOffsetThree-10) {
+  if ($w.scrollTop() > targetOffsetThree - 10) {
     $('#navHome').css({ "color": "azure" });
     $('#navVideo').css({ "color": "azure" });
     $('#navBasic').css({ "color": "burlywood" });
     $('#navFeatures').css({ "color": "azure" });
     $('#navContact').css({ "color": "azure" });
   }
-  if ($w.scrollTop() > targetOffsetFour-100) {
+  if ($w.scrollTop() > targetOffsetFour - 100) {
     $('#navHome').css({ "color": "azure" });
     $('#navVideo').css({ "color": "azure" });
     $('#navBasic').css({ "color": "azure" });
     $('#navFeatures').css({ "color": "burlywood" });
     $('#navContact').css({ "color": "azure" });
   }
-  if($w.scrollTop() + $(window).height() == $(document).height()) {
+  if ($w.scrollTop() + $(window).height() == $(document).height()) {
     $('#navHome').css({ "color": "azure" });
     $('#navVideo').css({ "color": "azure" });
     $('#navBasic').css({ "color": "azure" });
